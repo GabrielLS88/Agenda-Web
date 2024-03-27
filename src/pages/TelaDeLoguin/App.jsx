e368b0e09a1dcf282d57a905d6441901a5de4efd
Gabriel <lopes.biel2588@gmail.com>
2024-03-27 17:14:35 -0300

    Atualização

---
+++ src/pages/TelaDeLoguin/App.jsx
@@ -0,0 +1,14 @@
+import Header from '../../componets/Header/App'
+import Blocos from '../../componets/Blocos/App'
+import Progress from '../../componets/Progress/App';
+import './App.css'
+
+function Loguin() {
+  return (
+    <div className='bodyLoguin'>
+      
+    </div>
+  )
+}
+
+export default Loguin
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/App";
 import Cliente from "./pages/Cliente/App";
 import Horario from "./pages/Horario/App";
+import Loguin from "./pages/TelaDeLoguin/App";
 import React from "react";
 
 function MainRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Loguin />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/cliente" element={<Cliente />} />
       <Route path="/horario" element={<Horario />} />
     </Routes>
