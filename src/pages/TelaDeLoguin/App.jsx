7b9bb8113b3d761d375da29a2691759d721392ac
Gabriel <lopes.biel2588@gmail.com>
2024-04-16 00:18:46 -0300

    Update Rotas

--- src/pages/TelaDeLoguin/App.jsx
+++ src/pages/TelaDeLoguin/App.jsx
@@ -3,11 +3,26 @@ import Blocos from '../../componets/Blocos/App'
 import Progress from '../../componets/Progress/App';
 import './App.css'
 
+function verificarAcess() {
+  let userIp = document.getElementById("userIP").value
+  let password = document.getElementById("passwordIP").value
+  console.log(userIp, password)
+  if(userIp == "" || password == ""){
+    alert("Por gentileza tente nomavente!!" + "\n" + "Dados incompletos")
+  }else if(userIp === "Gab" && password === "123"){
+    localStorage.setItem("token", "true");
+    window.location.href = "/home";
+  }
+}
+
 function Loguin() {
   return (
     <div className='bodyLoguin'>
+      <h1 className='tituloLogin'>AGENDA CONNECT</h1>
       <div className="view">
-        
+        <input type="text" id='userIP' placeholder='Usuario' />
+        <input type="password" id='passwordIP' placeholder='Senha' />
+        <button id='btnLoguin' onClick={verificarAcess}>Entrar</button>
       </div>
     </div>
   )
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,38 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/App";
 import Cliente from "./pages/Cliente/App";
 import Horario from "./pages/Horario/App";
-import Loguin from "./pages/TelaDeLoguin/App";
-import React from "react";
+import Login from "../src/pages/TelaDeLoguin/App"; // Corrigido o nome do arquivo
+import React, { useState } from "react";
+
+// Função de verificação de autenticação simulada
+function verificarAutenticacao() {
+  // Substitua isso pela lógica real de verificação de autenticação
+  return localStorage.getItem("token") !== null; // Corrigido para verificar se o token não é nulo
+}
+
+// Componente de rota privada
+function PrivateRoute({ element, ...rest }) {
+  const [autenticado, setAutenticado] = useState(verificarAutenticacao());
+
+  console.log("Autenticado:", autenticado);
+
+  if (!autenticado && rest.path !== "/") {
+    console.log("Redirecionando para a tela de login...");
+    return <Navigate to="/" replace />;
+  }
+
+  return <Route {...rest} element={element} />;
+}
+
 
 function MainRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Loguin />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/cliente" element={<Cliente />} />
-      <Route path="/horario" element={<Horario />} />
+      <Route path="/" element={<Login />} /> {/* Corrigido o componente importado */}
+      <PrivateRoute path="/home" element={<Home />} />
+      <PrivateRoute path="/cliente" element={<Cliente />} />
+      <PrivateRoute path="/horario" element={<Horario />} />
     </Routes>
   );
 }
