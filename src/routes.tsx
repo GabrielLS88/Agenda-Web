734606605c12de330317c02a7c092559c7caffd7
Gabriel <lopes.biel2588@gmail.com>
2024-04-17 00:07:28 -0300

    upDTE

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,37 +2,34 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/App";
 import Cliente from "./pages/Cliente/App";
 import Horario from "./pages/Horario/App";
-import Login from "../src/pages/TelaDeLoguin/App"; // Corrigido o nome do arquivo
-import React, { useState } from "react";
+import Login from "./pages/TelaDeLoguin/App";
+import React, { useState, useEffect } from "react";
 
-// Função de verificação de autenticação simulada
 function verificarAutenticacao() {
-  // Substitua isso pela lógica real de verificação de autenticação
-  return localStorage.getItem("token") !== null; // Corrigido para verificar se o token não é nulo
+  return localStorage.getItem("token") !== null;
 }
 
-// Componente de rota privada
 function PrivateRoute({ element, ...rest }) {
-  const [autenticado, setAutenticado] = useState(verificarAutenticacao());
+  const [autenticado, setAutenticado] = useState(false);
 
-  console.log("Autenticado:", autenticado);
+  useEffect(() => {
+    setAutenticado(verificarAutenticacao());
+  }, []);
 
-  if (!autenticado && rest.path !== "/") {
-    console.log("Redirecionando para a tela de login...");
-    return <Navigate to="/" replace />;
+  if (!autenticado) {
+    return <Navigate to="/" />;
   }
 
   return <Route {...rest} element={element} />;
 }
 
-
 function MainRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} /> {/* Corrigido o componente importado */}
-      <PrivateRoute path="/home" element={<Home />} />
-      <PrivateRoute path="/cliente" element={<Cliente />} />
-      <PrivateRoute path="/horario" element={<Horario />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/cliente" element={<Cliente />} />
+      <Route path="/horario" element={<Horario />} />
     </Routes>
   );
 }
